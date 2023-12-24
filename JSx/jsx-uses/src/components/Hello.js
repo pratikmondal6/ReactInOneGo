0b1e2438b77881b14d700468e7e1dddfa0897c64
Pratik Mondal <pratikmondal6@hotmail.com>
2023-12-24 15:05:54 +0100

    jsx examp

---
+++ JSx/jsx-uses/src/components/Hello.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Hello = () => {
+/*  return <div>
+      Hello Jsx
+  </div>;*/
+
+    return React.createElement("div", null, "Hello without Jsx");
+
+
+
+}
+
+export default Hello;
