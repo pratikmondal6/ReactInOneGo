053f0679e382f285365fed3b0d3ad5e7c08f6747
Pratik Mondal <pratikmondal6@hotmail.com>
2023-12-24 18:49:32 +0100

    Props

---
+++ Props/props-uses/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropsComponent from './components/PropsComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <PropsComponent name="Pratik"/>
+     <PropsComponent name="Pulak"/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ Props/props-uses/src/components/PropsComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PropsComponent = (props) => {
+    return (
+        <div>
+            <h1>Props {props.name}</h1>
+        </div>
+    )
+}
+
+export default PropsComponent;
